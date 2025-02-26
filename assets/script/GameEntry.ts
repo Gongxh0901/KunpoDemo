229da9ede3d198ad05817d9606e36e472b7d6b40
宫欣海 <gongxinhai@kunpo.cc>
2025-02-26 11:12:02 +0800

    UI资源加载规则配置

--- assets/script/GameEntry.ts
+++ assets/script/GameEntry.ts
@@ -1,5 +1,6 @@
 import { Debug } from './Debug';
 import { cc, fgui, kunpo } from './header';
+import { UIPackageRegister } from './UIPackageRegister';
 const { ccclass, property, menu } = cc._decorator;
 @ccclass("GameEntry")
 @menu("kunpo/GameEntry")
@@ -22,12 +23,13 @@ export class GameEntry extends kunpo.CocosEntry {
         kunpo.Platform.deviceId = deviceId;
 
         Debug.Register();
+        UIPackageRegister.Register();
         this.loadResources();
     }
 
     private loadResources(): void {
         let paths: kunpo.IAssetConfig[] = [
-            { path: "ui", type: cc.Asset },
+            { path: "ui/manual", type: cc.Asset }, // 手动加载UI基础资源
             { path: "prefab", type: cc.Prefab },
             // { path: "icon", type: cc.SpriteFrame },
             // { path: "texture/6101/spriteFrame", type: cc.SpriteFrame, isFile: true },
@@ -37,6 +39,7 @@ export class GameEntry extends kunpo.CocosEntry {
         loader.start({
             configs: paths,
             complete: () => {
+                fgui.UIPackage.addPackage("ui/manual/Basics");
                 this.loadComplete();
             },
             fail: (msg: string, err: Error) => {
@@ -50,15 +53,13 @@ export class GameEntry extends kunpo.CocosEntry {
 
 
     private loadComplete(): void {
-        fgui.UIPackage.addPackage("ui/Basics");
-        fgui.UIPackage.addPackage("ui/Home");
-        fgui.UIPackage.addPackage("ui/Game");
-
-        this.root.active = false;
-        kunpo.WindowManager.showWindow("HomeWindow", "这是一个测试窗口");
-
         kunpo.GlobalEvent.add("event::111", () => {
             kunpo.log("接收到事件");
         }, this);
+
+        kunpo.WindowManager.showWindow("HomeWindow", "这是一个测试窗口").then(() => {
+            kunpo.log("窗口显示成功");
+            this.root.active = false;
+        });
     }
 }
