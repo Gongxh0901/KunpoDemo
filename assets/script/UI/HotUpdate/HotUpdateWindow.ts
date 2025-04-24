3699f81f6d75c11dbcca08260d95baed07067856
gongxh <gongxinhai@kunpo.cc>
2025-04-24 18:00:14 +0800

    热更新修改

--- assets/script/UI/HotUpdate/HotUpdateWindow.ts
+++ assets/script/UI/HotUpdate/HotUpdateWindow.ts
@@ -46,22 +46,23 @@ export class HotUpdateWindow extends kunpo.Window {
         this.refreshTips("正在检查更新...  请稍后", false);
 
         kunpo.HotUpdateManager.getInstance().checkUpdate().then((res: kunpo.ICheckUpdatePromiseResult) => {
-            kunpo.log("检查热更新完成 结果:", JSON.stringify(res));
-            if (res.code == HotUpdateCode.Succeed) {
-                this.refreshTips(`发现热更新 需更新大小:${Math.floor(res.size / 1024 * 1000) * 0.001}MB`, true);
-                kunpo.WindowManager.showWindowIm("AlertWindow", {
-                    title: "提示",
-                    content: `发现热更新 需更新大小:${Math.floor(res.size / 1024 * 1000) * 0.001}MB`,
-                    okTitle: "更新",
-                    cancelTitle: "取消",
-                    complete: () => {
-                        this.startUpdate(true);
-                    },
-                    cancel: () => {
-                        kunpo.log("取消");
-                    },
-                });
-            } else if (res.code == HotUpdateCode.LatestVersion) {
+            kunpo.log("发现热更新:", JSON.stringify(res));
+            this.refreshTips(`发现热更新 需更新大小:${Math.floor(res.size / 1024 * 1000) * 0.001}MB`, true);
+            kunpo.WindowManager.showWindowIm("AlertWindow", {
+                title: "提示",
+                content: `发现热更新 需更新大小:${Math.floor(res.size / 1024 * 1000) * 0.001}MB`,
+                okTitle: "更新",
+                cancelTitle: "取消",
+                complete: () => {
+                    this.startUpdate(true);
+                },
+                cancel: () => {
+                    kunpo.log("取消");
+                },
+            });
+        }).catch((res: any) => {
+            log("检查热更新出错了", JSON.stringify(res));
+            if (res.code == HotUpdateCode.LatestVersion) {
                 this.refreshTips(`已经是最新版本了`, true);
                 kunpo.WindowManager.showWindowIm("AlertWindow", {
                     title: "提示",
@@ -76,8 +77,6 @@ export class HotUpdateWindow extends kunpo.Window {
                     okTitle: "知道了",
                 });
             }
-        }).catch((err: any) => {
-            log("检查热更新出错了", JSON.stringify(err));
         });
     }
 
@@ -93,7 +92,6 @@ export class HotUpdateWindow extends kunpo.Window {
             skipCheck: skipCheck,
             progress: (kb: number, total: number) => {
                 kunpo.log("热更新进度", kb, total);
-
                 this.refreshTips(`正在更新...  请稍后  ${Math.floor(kb / total * 100)}% `, false);
             },
             complete: (code: HotUpdateCode, message: string) => {
@@ -119,6 +117,13 @@ export class HotUpdateWindow extends kunpo.Window {
                         cancel: () => {
                             kunpo.log("取消");
                         },
+                    });
+                } else if (code == HotUpdateCode.LoadVersionFailed || code == HotUpdateCode.ParseVersionFailed) {
+                    this.refreshTips(`更新失败了 code:${code} message:${message}`, true);
+                    kunpo.WindowManager.showWindowIm("AlertWindow", {
+                        title: "提示",
+                        content: `更新失败了 code:${code} message:${message} 可以选择跳过热更新`,
+                        okTitle: "知道了",
                     });
                 } else {
                     this.refreshTips(`更新失败了 code:${code} message:${message}`, true);
